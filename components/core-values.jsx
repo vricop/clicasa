a44798c8a8e6515db113b60aa2aae75158283fdb
Víctor Rico <vricop@gmail.com>
2021-09-17 18:20:01 +0200

    Add <CoreValues /> component

---
+++ components/core-values.jsx
@@ -0,0 +1,71 @@
+import Svg from './svg'
+
+export default function CoreValues({ children }) {
+  return (
+    <section>
+      <div className="container core-values">
+        <div className="core-values__header">
+          <h2 className="core-values__header__heading">
+            En Clicasa sólo hay ventajas
+          </h2>
+          <p className="core-values__header__tagline">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+        <div className="core-values-items">
+          <div className="core-values-items__item">
+            <Svg
+              className="core-values-items__item__icon"
+              icon="time"
+              width="3.75em"
+            />
+            <h3 className="core-values-items__item__title">Oferta en 24 h</h3>
+            <p className="core-values-items__item__description">
+              No te preocupes de su estado, ni de reparaciones para eneseñarlo
+            </p>
+          </div>
+          <div className="core-values-items__item">
+            <Svg
+              className="core-values-items__item__icon"
+              icon="wallet"
+              width="3.75em"
+            />
+            <h3 className="core-values-items__item__title">Pago inmediato</h3>
+            <p className="core-values-items__item__description">
+              No te preocupes de su estado, ni de reparaciones para eneseñarlo
+            </p>
+          </div>
+          <div className="core-values-items__item">
+            <Svg
+              className="core-values-items__item__icon"
+              icon="paperplane"
+              width="3.75em"
+            />
+            <h3 className="core-values-items__item__title">
+              Ahórrate el papeleo
+            </h3>
+            <p className="core-values-items__item__description">
+              Si lo necesitas también podemos ayudarte a encontrar tu nuevo
+              hogar
+            </p>
+          </div>
+          <div className="core-values-items__item">
+            <Svg
+              className="core-values-items__item__icon"
+              icon="calendar"
+              width="3.75em"
+            />
+            <h3 className="core-values-items__item__title">
+              Tu eliges la fecha
+            </h3>
+            <p className="core-values-items__item__description">
+              Si lo necesitas también podemos ayudarte a encontrar tu nuevo
+              hogar
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
+import CoreValues from '../components/core-values'
 import HeroHeader from '../components/hero-header'
 
 export default function Home() {
-  return <HeroHeader />
+  return (
+    <>
+      <HeroHeader />
+      <CoreValues />
+    </>
+  )
 }
